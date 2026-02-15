9d6de33d15a9ff3a5753fbd5e6d529a9c5eb6b26
Pendragonnnnn <chansothymonthai01@gmail.com>
2026-02-15 14:42:23 +0700

    complete

---
+++ W5 - PROPS/Start code/exercise-5/src/data.js
@@ -0,0 +1,47 @@
+/* Your data here */
+import diceImg from "./assets/dice.png";
+import cardImg from "./assets/card.png";
+import diamondImg from "./assets/diamons.png";
+import horseImg from "./assets/horse.png";
+import minesweeperImg from "./assets/minesweeper.png"
+import plinkoImg from "./assets/plinko.png";
+
+export const ALL_CARDS = [
+    {
+        name: "Dice",
+        image: {
+            src: diceImg,
+        },
+
+    },
+    {
+        name: "Card",
+        image: {
+            src: cardImg,
+        },
+    },
+    {
+        name: "Diamond",
+        image: {
+            src: diamondImg,
+        },
+    },
+    {
+        name: "Horse",
+        image: {
+            src: horseImg,
+        },
+    },
+    {
+        name: "Mine Sweeper",
+        image: {
+            src: minesweeperImg,
+        },
+    },
+    {
+        name: "Plinko",
+        image: {
+            src: plinkoImg,
+        },
+    },
+];
